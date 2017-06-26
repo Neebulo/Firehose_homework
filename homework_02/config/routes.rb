4a144bf70db614de01b40b6176b044f83ecc3cab
neebulo <rhapsodyshadow@yahoo.com>
2017-06-26 08:32:03 +0000

    routes.rb | added root and resources for CRUD routes and root index

--- homework_02/config/routes.rb
+++ homework_02/config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/index'
-
-  get 'books/new'
-
-  get 'books/create'
-
-  get 'books/show'
-
-  get 'books/edit'
-
-  get 'books/update'
-
-  get 'books/destroy'
+  root 'books#index'
+  resources :books
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
